250b4574dabdda678d9754bae540ed5118aeba28
sky line <32512284+linesky@users.noreply.github.com>
2024-06-14 21:39:16 +0100

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,21 @@
+import re
+s='hello "world","hi","there";'
+def processos(expressao):
+    # Dividir a expressão em tokens
+    tokens = re.findall(r'\"(.*?)\"', expressao)
+
+    
+    for token in tokens:
+        print (token)
+
+    return 
+
+def main():
+    
+    processo =processos(s)
+    
+
+print("\x1b[43;37m")
+if __name__ == "__main__":
+    main()
+
