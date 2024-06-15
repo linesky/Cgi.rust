aaa8993eeebbaaa69187c301e420048dc1be32df
sky line <32512284+linesky@users.noreply.github.com>
2024-06-15 01:58:44 +0100

    Add files via upload

--- time.py
+++ time.py
@@ -2,8 +2,8 @@ import re
 s='08:09:10'
 def processos(expressao):
     # Dividir a expressão em tokens
-    regExp= r'([\d]+)\:*'
-    tokens = re.findall(regExp,expressao)
+    regExp= r'[:\-\.]'
+    tokens = re.split(regExp,expressao)
     for n in tokens:
         print(n)
     
